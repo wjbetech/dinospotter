03d24ad933475eae39dfb7ea15d2e05472648360
wjbetech <wjbetech@gmail.com>
2026-09-08 12:47:23 +0900

    chore: cleaning up globe/index.ts and adding docs from docs branch

--- apps/website/src/components/globe/index.ts
+++ apps/website/src/components/globe/index.ts
@@ -1,12 +1,8 @@
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
-
-import { setWorkerUrl } from "maplibre-gl";
-
-setWorkerUrl(workerUrl);
-
 export async function initGlobe(el: HTMLElement) {
   await import("maplibre-gl/dist/maplibre-gl.css");
-  const { Map } = await import("maplibre-gl");
+  const { Map, setWorkerUrl } = await import("maplibre-gl");
+  const workerUrl = (await import("maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url")).default;
+  setWorkerUrl(workerUrl);
 
   const map = new Map({
     container: el,
